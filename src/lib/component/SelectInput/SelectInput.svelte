798f8a6867f31643f191f408a961a4dc8799006f
329henry <tksssp329@gmail.com>
2023-02-07 00:44:23 +0800

    - update select input and config

--- src/lib/component/SelectInput/SelectInput.svelte
+++ src/lib/component/SelectInput/SelectInput.svelte
@@ -1,34 +1,89 @@
-<script>
+<script lang="ts">
+  import ArrowDown from './assets/icon-arrow-down.svg'
+
   const optionList = [
-    { value: 1, label: 'San Serif' },
-    { value: 2, label: 'Serif' },
-    { value: 3, label: 'Mono' }
+    { value: 'San Serif', label: 'San Serif' },
+    { value: 'Serif', label: 'Serif' },
+    { value: 'Mono', label: 'Mono' }
   ]
 
   let selected = optionList[0].value
-</script>
+  let isOpenDropdown = false
 
-<select bind:value={selected}>
-  {#each optionList as option}
-    <option value={option.value}>{option.label}</option>
-  {/each}
-</select>
-
-<style>
-  select {
-    width: 118px;
-    background: url(../../../assets/images/icon-arrow-down.svg) no-repeat right
-      transparent;
-    font-size: 18px;
-    font-weight: 700;
-    border: none;
-    appearance: none;
-    -moz-appearance: none;
-    -webkit-appearance: none;
+  function toggleDropdown() {
+    isOpenDropdown = !isOpenDropdown
   }
 
-  option {
+  function onClickOption(value: string) {
+    selected = value
+    isOpenDropdown = false
+  }
+</script>
+
+<div class="select-container">
+  <div class="selected" on:click={toggleDropdown}>
+    <div>{selected}</div>
+    <img class="arrow-down" src={ArrowDown} alt="click-dropdown" />
+  </div>
+  {#if isOpenDropdown}
+    <div class="dropdown">
+      {#each optionList as option}
+        <div class="option" on:click={() => onClickOption(option.value)}>
+          {option.value}
+        </div>
+      {/each}
+    </div>
+  {/if}
+</div>
+
+<style lang="scss">
+  .select-container {
+    width: 118px;
+    height: 24px;
+    position: relative;
+  }
+
+  .selected {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 18px;
+    font-weight: 700;
+  }
+
+  .arrow-down {
+    width: 12px;
+    height: 6px;
+  }
+
+  .dropdown {
     width: 183px;
     height: 152px;
+    position: absolute;
+    box-sizing: border-box;
+    top: 25px;
+    right: 0px;
+    padding: 24px;
+    background-color: #1f1f1f;
+    box-shadow: 0px 5px 30px #a445ed;
+    border-radius: 16px;
+    z-index: 1;
+  }
+
+  .option {
+    width: 90%;
+    height: 24px;
+    font-size: 18px;
+    font-weight: 700;
+    text-align: left;
+
+    &:nth-child(2) {
+      font-family: 'Lora', serif;
+      padding: 16px 0;
+    }
+
+    &:last-child {
+      font-family: 'Inconsolata', monospace;
+    }
   }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess(),
+      onwarn: (warning, handler) => {
+        // disable a11y warnings
+        if (warning.code.startsWith('a11y-click-events-have-key-events')) return
+        handler(warning)
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      $components: path.resolve(__dirname, './src/components'),
+      $constants: path.resolve(__dirname, './src/constants'),
+      $feature: path.resolve(__dirname, './src/feature'),
+      $helpers: path.resolve(__dirname, './src/helpers'),
+      $routes: path.resolve(__dirname, './src/routes'),
+      $types: path.resolve(__dirname, './src/types'),
+      $utils: path.resolve(__dirname, './src/utils'),
+      $api: path.resolve(__dirname, './src/api')
+    }
+  }
 })
